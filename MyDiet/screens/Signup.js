8f3ea943566bf1370692f00fad86405eef52f21a
fizzy-1111 <62971763+fizzy-1111@users.noreply.github.com>
2022-01-03 02:33:24 +0700

    Revert "Sign Up fix"
    This reverts commit fcf4bd5c73b9a66fdda0384e25792c41ea79f62f.

--- MyDiet/screens/Signup.js
+++ MyDiet/screens/Signup.js
@@ -93,7 +93,7 @@ const SignUp = () => {
         <View  style={styles.safeview} >
         <TouchableOpacity style={styles.button} onPress={onCreateAccount}>
           <Text style={styles.textStyle3}>
-            <B>Create Account</B>
+            <B>Next</B>
           </Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.button2} onPress={onPress}>
